786b8a406040196fd3fe13a561b36013cfab047e
aleksey.permyakov <liosha.89@gmail.com>
2014-05-20 11:12:15 +0300

    ANNOTATIONJAVA-38: Atmosphere is not work in Java Web Sample for GroupDocs.Annotation

--- src/com/groupdocs/annotation/samples/javaweb/GetDocumentCollaboratorsHandlerServlet.java
+++ src/com/groupdocs/annotation/samples/javaweb/GetDocumentCollaboratorsHandlerServlet.java
@@ -1,24 +1,26 @@
 package com.groupdocs.annotation.samples.javaweb;
 
-import java.io.IOException;
+import org.apache.log4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 
 /**
  *
  * @author imy
  */
-public class GetAvatarHandlerServlet extends AnnotationServlet {
-
+public class GetDocumentCollaboratorsHandlerServlet extends AnnotationServlet{
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setHeader("Content-type", "application/json;charset=UTF-8");
-        response.getOutputStream().write(annotationHandler.getPdfVersionOfDocumentHandler(request, response).toString().getBytes());
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        response.setHeader("Content-type", "application/json;charset=UTF-8");
+        response.getOutputStream().write(annotationHandler.getDocumentCollaboratorsHandler(request, response).toString().getBytes());
     }
 }
